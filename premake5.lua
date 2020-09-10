6e5ffac20d7a2bbd6347ce4f451205253ba46254
omrkocar <kocartahir@gmail.com>
2020-09-10 12:25:43 -0400

    Finished building project and added WinMain.cpp

---
+++ premake5.lua
@@ -0,0 +1,19 @@
+workspace "Game"
+	configurations { "Debug", "Release" }
+	location "build"
+
+	platforms { "x64" }
+	characterset "MBCS"
+
+
+
+project "Game"
+
+	kind "WindowedApp"
+
+	files {
+		"Game/Source/**.cpp",
+		"GenerateProjectFiles.bat",
+		"premake5.lua",
+		".gitignore"
+	}
