f18495c1651061275869e0d65539a81d0f719730
omrkocar <kocartahir@gmail.com>
2020-09-15 13:00:31 -0400

    Added the Framework and updated premake lua

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,6 @@
 workspace "Game"
 	configurations { "Debug", "Release" }
 	location "build"
-
 	platforms { "x64" }
 	characterset "MBCS"
 
@@ -9,11 +8,37 @@ workspace "Game"
 
 project "Game"
 
-	kind "WindowedApp"
+	kind		"WindowedApp"
+	location	"build/Game"
 
 	files {
 		"Game/Source/**.cpp",
+		"Game/Source/**.h",
 		"GenerateProjectFiles.bat",
 		"premake5.lua",
 		".gitignore"
+	}
+
+	includedirs {
+		"Framework/Source",
+		"Game/Source",
+	}
+
+	links {
+		"Framework",
+		"opengl32",
+	}
+
+project "Framework"
+	kind		"StaticLib"
+	location	"build/Framework"
+
+	files {
+		"Framework/Source/**.cpp",
+		"Framework/Source/**.h",
+		
+	}
+
+	includedirs {
+		"Framework/Source",
 	}
